3588be9a5809ce68663d13b31a2e346dc569d50a
Marc <marcfont12@gmail.com>
2020-05-04 16:22:55 +0200

    frontend amb error

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 
-
 App = {
   web3Provider: null,
   contracts: {},
@@ -36,6 +35,8 @@ App = {
     //$(document).on('click', '.btn-adopt', App.handleAdopt);
     $(document).on('click', '.btn-read', App.readCar);
     $(document).on('click', '.btn-write', App.writeCar);
+    
+
   },
 
   writeCar: function () {
@@ -50,14 +51,28 @@ App = {
   },
 
   readCar: function () {
+   
+
+
+
+
     var ClearMileageInstance;
     App.contracts.ClearMileage.deployed().then(function (instance) {
       ClearMileageInstance = instance;
 
       //ClearMileageInstance.setCarInfo("MATR5", 53000, {from: "0xA129526f45e080064a7d4893F70F1e999Bd2bc44"});
       //ClearMileageInstance.getCarInfoMatricula("0xA129526f45e080064a7d4893F70F1e999Bd2bc44").then(function(v){console.log(v)})
-      ClearMileageInstance.getCarInfo(document.getElementById("walletInput").value).then(function (v) {
+      ClearMileageInstance.getCarInfo(document.getElementById("VINInput").value).then(function (v) {
         console.log(v)
+        
+        if (v.matricula == "ERROR. Car doesn't exist") {
+          var gapians = document.getElementById("texterror").style.display = "block";
+         
+        }
+        else {
+        var gapians = document.getElementById("texterror").style.display = "none";
+        var ContainerWeb = document.getElementById("tot1").style.display = "none";
+        var Grafic = document.getElementById("myChart").style.display = "table";
         for (let i = 0; i < v.kmArray.length; ++i) {
           //var date = new Date((v.kmArray[i].date) * 1000 + 3.154e+7*1000*i);
           var date = new Date((v.kmArray[i].date) * 1000 + 604800*1000*i);
@@ -69,12 +84,14 @@ App = {
           //v.kmArray[i].km = km;
           //document.getElementById("testP").innerHTML = "KEK";
         }
-        document.getElementById("testP").innerHTML = "Matricula: "+ v.matricula;
-        
+        document.getElementById("testP").innerHTML = v.matricula;
+        document.getElementById("testP").classList.add("matricula")
+
         console.log("testArray")
           console.log(v.kmArray.map(e => {return {t:e.date, y:e.km}}))
           var timeFormat = 'MM/DD/YYYY HH:mm';
-          var ctx = document.getElementById('myChart').getContext('2d');
+          var ctx = document.getElementById('myChart').getContext('2d'); 
+         // ctx.style.backgroundColor = 'rgba(255,0,0,255)';
           var myChart = new Chart(ctx, {
             type: 'line',
             //data: v.kmArray.map(e => {return {t:new Date(e.date*1000), y:e.km}}),
@@ -82,37 +99,75 @@ App = {
               labels: v.kmArray.map(e => {return e.date}),
               datasets: [{
                 label: 'Km',
+               // backgroundColor: "rgba(52,152,219,0)",
                 //backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-                borderColor: 'blue',
+                borderColor: '#ffc107',
                 fill: false,
                 data: v.kmArray.map(e => {return {t:e.date, y:e.km}}),
                 lineTension: 0
               }]
             },
             options: {
+              tooltips: {
+                displayColors: false
+              },
+              legend: {
+                labels: {
+                    fontColor: "white"
+                }
+              },
               responsive: true,
               title: {
                 text: 'Kilometraje'
               },
               scales: {
                 xAxes: [{
+                  ticks: {
+                      beginAtZero: true,
+                      fontColor: '#FFF',
+                      fontSize: 15,
+                      fontStyle: 'italic',
+                  },
                   type: 'time',
                   time: {
                     stepSize: 10
                   },
                   scaleLabel: {
                     display: true,
-                    labelString: 'Date'
+                    labelString: 'Date',
+                    fontColor: "white"
+                  },
+                  gridLines: {
+                    display: true,
+                    drawOnChartArea: false,
+                    zeroLineWidth: 1,
+                    zeroLineColor: "white",
+                    color:"white"
                   }
                 }],
                 yAxes: [{
+                  ticks: {
+                    beginAtZero: true,
+                    fontColor: '#FFF',
+                    fontSize: 15,
+                    fontStyle: 'bold',
+                },
                   scaleLabel: {
-                    display: true
+                    display: true,
+                    fontColor:"white"
+                  },
+                  gridLines: {
+                    display: true,
+                    drawOnChartArea: false,
+                    zeroLineWidth: 1,
+                    zeroLineColor: "white",
+                    color:"white"
                   }
                 }]
               },
             }
           });
+        }
         console.log(v)
       })
     })
